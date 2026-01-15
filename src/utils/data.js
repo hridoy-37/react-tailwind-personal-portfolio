9ebe7a23695d40ed0c1001fd864297ac5c17dbb8
Hridoy <hridoy_37@outlook.com>
2026-01-15 21:16:08 +0600

    updated image details for testimonials

--- src/utils/data.js
+++ src/utils/data.js
@@ -44,7 +44,7 @@ export const TESTIMONIALS = [
     author: "Estiak Ahmed",
     role: "CTO, RaadCraft Solutions",
     avatar:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=100&h=100&fit=crop",
+      "/public/rumi_bhai.webp",
   },
   {
     quote:
